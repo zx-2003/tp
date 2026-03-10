ab061ee0872ebfbfba2dcb13fd1f74402451f485
Keiran <keinn2210@gmail.com>
2026-03-11 00:06:34 +0800

    Fix checkstyle violations

--- src/main/java/seedu/tutor/logic/commands/HelpCommand.java
+++ src/main/java/seedu/tutor/logic/commands/HelpCommand.java
@@ -12,17 +12,17 @@ public class HelpCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Available commands:\n" +
-            "add n/NAME p/PHONE e/email a/address \n " +
-            "e.g., add n/James Ho p/22224444 e/jamesho@example.com " +
-            "a/123, Clementi Rd, 1234665 t/friend t/colleague \n" +
-            "delete INDEX e.g., delete 3 \n" +
-            "list \n" +
-            "clear \n" +
-            "find KEYWORD [MORE KEYWORDS] e.g., find James Jake \n" +
-            "help \n" +
-            "edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [t/TAG]..." +
-            "e.g.,edit 2 n/James Lee e/jameslee@example.com";
+    public static final String SHOWING_HELP_MESSAGE = "Available commands:\n"
+            + "add n/NAME p/PHONE e/email a/address \n"
+            + "e.g., add n/James Ho p/22224444 e/jamesho@example.com"
+            + "a/123, Clementi Rd, 1234665 t/friend t/colleague \n"
+            + "delete INDEX e.g., delete 3 \n"
+            + "list \n"
+            + "clear \n"
+            + "find KEYWORD [MORE KEYWORDS] e.g., find James Jake \n"
+            + "help \n"
+            + "edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [t/TAG]..."
+            + "e.g.,edit 2 n/James Lee e/jameslee@example.com";
 
     @Override
     public CommandResult execute(Model model) {
